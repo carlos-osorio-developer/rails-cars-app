6dd9a9d96cf9bf03ecec6ba3bf913c733d9adf4c
carlos-osorio-developer <osorio.uis@gmail.com>
2022-05-14 17:31:18 -0500

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :address, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
 end
