8be6d69a1cf568bfb7f6f293df685b185221c062
carlos-osorio-developer <osorio.uis@gmail.com>
2022-05-14 11:41:54 -0500

    Add MVC and tests for Cars, set PostgreSQL as db manager

---
+++ spec/views/cars/edit.html.erb_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "cars/edit", type: :view do
+  before(:each) do
+    @car = assign(:car, Car.create!(
+      name: "MyString",
+      description: "MyText",
+      image_url: "MyString",
+      model: 1,
+      hourly_rate: 1.5,
+      user: nil
+    ))
+  end
+
+  it "renders the edit car form" do
+    render
+
+    assert_select "form[action=?][method=?]", car_path(@car), "post" do
+
+      assert_select "input[name=?]", "car[name]"
+
+      assert_select "textarea[name=?]", "car[description]"
+
+      assert_select "input[name=?]", "car[image_url]"
+
+      assert_select "input[name=?]", "car[model]"
+
+      assert_select "input[name=?]", "car[hourly_rate]"
+
+      assert_select "input[name=?]", "car[user_id]"
+    end
+  end
+end
---
+++ spec/views/cars/show.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "cars/show", type: :view do
+  before(:each) do
+    @car = assign(:car, Car.create!(
+      name: "Name",
+      description: "MyText",
+      image_url: "Image Url",
+      model: 2,
+      hourly_rate: 3.5,
+      user: nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/Image Url/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(/3.5/)
+    expect(rendered).to match(//)
+  end
+end
