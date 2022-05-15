a4aba43979cb1b5b2a7cb13571f7cddace87375d
carlos-osorio-developer <osorio.uis@gmail.com>
2022-05-14 21:24:29 -0500

    Add test to validate uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :address, presence: true, uniqueness: true
   validates :phone, presence: true, uniqueness: true
 end
